db79bb96712208fe85689f31ab71716840a761dc
Ddaaan <de2572@naver.com>
2025-10-19 23:05:41 +0900

    style: 코드 들여쓰기 수정

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -6,9 +6,9 @@ import java.math.BigDecimal;
 import java.math.RoundingMode;
 
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("덧셈할 문자열을 입력해주세요.");
-        String input = Console.readLine();
+  public static void main(String[] args) {
+    System.out.println("덧셈할 문자열을 입력해주세요.");
+    String input = Console.readLine();
 
     AddCalculator calculator = new AddCalculator();
     BigDecimal result = calculator.add(input);
